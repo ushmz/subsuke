16bf8d57e0f8fbee3356384835a8516d89038f3c
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2021-04-04 01:43:17 +0900

    :label: Add model class

---
+++ lib/models/subsucription.dart
@@ -0,0 +1,14 @@
+
+class Subscription {
+	final String name;
+	final DateTime billingAt;
+	final int price;
+	final String cycle;
+
+	Subscription({
+		this.name,
+		this.billingAt,
+		this.price,
+		this.cycle
+	});
+}
