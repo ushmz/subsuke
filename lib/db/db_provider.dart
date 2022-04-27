052e9b8200f402756335475f60ebbd927ad65f22
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-04-28 06:11:08 +0900

    :card_file_box: Add database

---
+++ lib/db/db_provider.dart
@@ -0,0 +1,105 @@
+import 'dart:io';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:subsuke/models/subsc.dart';
+
+class DBProvider {
+  final _name = 'subscriptions.db';
+  final _version = 1;
+  final _tablename = 'subscriptions';
+
+  DBProvider._();
+  static final DBProvider? instance = DBProvider._();
+
+  static Database? _database;
+  Future<Database?> get database async {
+    if (_database != null) return _database;
+    _database = await _initDB();
+    return _database;
+  }
+
+  Future<String> getDBPath() async {
+    var dbDirPath = "";
+    // See https://zenn.dev/beeeyan/articles/b9f1b42de9cb67
+    if (Platform.isAndroid) {
+      dbDirPath = await getDatabasesPath();
+    } else if (Platform.isIOS) {
+      final dbDirectory = await getLibraryDirectory();
+      dbDirPath = dbDirectory.path;
+    } else {
+      throw Exception("Unknown platform");
+    }
+    return join(dbDirPath, _name);
+  }
+
+  _initDB() async {
+    final path = await getDBPath();
+    return await openDatabase(
+      path,
+      version: _version,
+      onCreate: (db, version) async {
+        var batch = db.batch();
+        // [TODO] Add note
+        batch.execute('''
+            CREATE TABLE subscriptions(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                price INTEGER NOT NULL,
+                next TEXT NOT NULL,
+                interval INTEGER NOT NULL,
+          )
+        ''');
+        await batch.commit();
+      },
+      onDowngrade: onDatabaseDowngradeDelete,
+      singleInstance: true,
+    );
+  }
+
+  Future<void> dropTable() async {
+    final db = await database;
+    final _ = await db?.execute("DROP TABLE $_tablename");
+    return;
+  }
+
+  Future<void> createSubscriptionItem(SubscriptionItem item) async {
+    final db = await database;
+    final _ = await db?.insert(_tablename, item.toInsertMap());
+    return;
+  }
+
+  Future<List<SubscriptionItem>> getAllSubscriptions() async {
+    final db = await database;
+    final res = await db?.query(_tablename);
+    if (res == null) {
+      return [];
+    }
+    List<SubscriptionItem> list = res.isNotEmpty
+        ? res.map((e) => SubscriptionItem.fromMap(e)).toList()
+        : [];
+    return list;
+  }
+
+  Future<void> updateSubscriptionItem(SubscriptionItem item) async {
+    final db = await database;
+    final _ = await db?.update(
+      _tablename,
+      item.toInsertMap(),
+      where: "id = ?",
+      whereArgs: [item.id],
+    );
+    return;
+  }
+
+  Future<void> deleteSubscriptionItem(int id) async {
+    final db = await database;
+    final _ = await db?.delete(
+      _tablename,
+      where: "id = ?",
+      whereArgs: [id],
+    );
+    return;
+  }
+}
---
+++ lib/models/subsc.dart
@@ -0,0 +1,144 @@
+class SubscriptionItem {
+  final int id;
+  final String name;
+  final int price;
+  final DateTime next;
+  final PaymentInterval interval;
+  /* final String note; */
+
+  const SubscriptionItem(
+    this.id,
+    this.name,
+    this.price,
+    this.next,
+    this.interval,
+    /* this.note, */
+  );
+
+  int get getID => id;
+
+  // [TODO] Ulid is String value
+  // See : https://pub.dev/packages/ulid
+  /* static assignUUID() { */
+  /*     id = Ulid(); */
+  /* } */
+
+  Map<String, dynamic> toMap() => {
+        "id": id,
+        "name": name,
+        "price": price,
+        "next": next.toUtc().toIso8601String(),
+        "interval": interval.intervalID,
+        /* "note": note */
+      };
+
+  Map<String, dynamic> toInsertMap() => {
+        "name": name,
+        "price": price,
+        "next": next.toUtc().toIso8601String(),
+        "interval": interval.intervalID,
+        /* "note": note, */
+      };
+
+  factory SubscriptionItem.fromMap(Map<String, dynamic> json) =>
+      SubscriptionItem(
+        json['id'],
+        json['name'],
+        json['price'],
+        DateTime.parse(json['next']).toLocal(),
+        getPaymentInterval(json['interval']),
+        /* json['note'], */
+      );
+
+  SubscriptionItem updateNextPayment() {
+    switch (interval) {
+      case PaymentInterval.Daily:
+        return SubscriptionItem(
+          id,
+          name,
+          price,
+          DateTime(next.year, next.month, next.day + 1),
+          interval,
+          /* note, */
+        );
+      case PaymentInterval.Weekly:
+        return SubscriptionItem(
+          id,
+          name,
+          price,
+          DateTime(next.year, next.month, next.day + 7),
+          interval,
+          /* note, */
+        );
+      case PaymentInterval.Fortnightly:
+        return SubscriptionItem(
+          id,
+          name,
+          price,
+          DateTime(next.year, next.month, next.day + 14),
+          interval,
+          /* note, */
+        );
+      case PaymentInterval.Monthly:
+        return SubscriptionItem(
+          id,
+          name,
+          price,
+          DateTime(next.year, next.month + 1, next.day),
+          interval,
+          /* note, */
+        );
+      case PaymentInterval.Yearly:
+        return SubscriptionItem(
+          id,
+          name,
+          price,
+          DateTime(next.year + 1, next.month, next.day),
+          interval,
+          /* note, */
+        );
+    }
+  }
+}
+
+enum PaymentInterval {
+  Daily,
+  Weekly,
+  Fortnightly,
+  Monthly,
+  Yearly,
+}
+
+extension PaymentIntervalExt on PaymentInterval {
+  int get intervalID {
+    switch (this) {
+      case PaymentInterval.Daily:
+        return 1;
+      case PaymentInterval.Weekly:
+        return 2;
+      case PaymentInterval.Fortnightly:
+        return 3;
+      case PaymentInterval.Monthly:
+        return 4;
+      case PaymentInterval.Yearly:
+        return 5;
+    }
+  }
+}
+
+PaymentInterval getPaymentInterval(int id) {
+  switch (id) {
+    case 1:
+      return PaymentInterval.Daily;
+    case 2:
+      return PaymentInterval.Weekly;
+    case 3:
+      return PaymentInterval.Fortnightly;
+    case 4:
+      return PaymentInterval.Monthly;
+    case 5:
+      return PaymentInterval.Yearly;
+    default:
+      return PaymentInterval.Monthly;
+  }
+}
