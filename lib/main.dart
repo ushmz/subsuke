361fd9d8fe97bae3f794dc2210665694207950bc
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-04-30 06:34:16 +0900

    :art: Fix colors

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:subsuke/blocs/edit_screen_bloc.dart';
 import 'package:subsuke/blocs/pagination_bloc.dart';
 import 'package:subsuke/blocs/settings_bloc.dart';
 import 'package:subsuke/blocs/subscription_item_bloc.dart';
@@ -29,32 +28,53 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
           brightness: Brightness.light,
           primaryColor: Color(0xFF8B4492),
-          primarySwatch: Colors.purple,
-          scaffoldBackgroundColor: Colors.white,
-          bottomAppBarColor: Color(0xFF8B4492),
           hintColor: Colors.black54,
+          hoverColor: Colors.black54,
+          backgroundColor: Color(0xFFFFFFFF),
+          scaffoldBackgroundColor: Color(0xFFF2F2F6),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Color(0xFFFFFFFF),
+              foregroundColor: Color(0xFF000000)),
+          bottomAppBarColor: Color(0xFFFFFFFF),
           iconTheme: IconThemeData(color: Colors.black),
+          cardTheme: CardTheme(
+              color: Color(0xFFFFFFFF),
+              clipBehavior: Clip.antiAlias,
+              shadowColor: Colors.white10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
           textTheme: TextTheme(
-            bodyText1: TextStyle(color: Colors.black, fontSize: 24),
-            bodyText2: TextStyle(color: Colors.black54, fontSize: 18),
+            bodyLarge: TextStyle(color: Colors.black, fontSize: 32),
+            bodyMedium: TextStyle(color: Colors.black, fontSize: 24),
+            bodySmall: TextStyle(color: Colors.black, fontSize: 18),
           )),
       darkTheme: ThemeData(
           brightness: Brightness.dark,
           primaryColor: Color(0xFF9A57A2),
-          primarySwatch: Colors.deepPurple,
-          backgroundColor: Color(0xFF252428),
-          scaffoldBackgroundColor: Color(0xFF252428),
-          bottomAppBarColor: Color(0xFF8B4492),
           hintColor: Colors.white54,
+          hoverColor: Colors.white54,
+          backgroundColor: Color(0xFF1C1C1E),
+          scaffoldBackgroundColor: Color(0xFF000000),
+          appBarTheme: AppBarTheme(
+              backgroundColor: Color(0xFF212121),
+              foregroundColor: Color(0xFFFFFFFF)),
+          bottomAppBarColor: Color(0xFF212121),
           iconTheme: IconThemeData(color: Colors.white),
+          cardTheme: CardTheme(
+              color: Color(0xFF1C1C1E),
+              clipBehavior: Clip.antiAlias,
+              shadowColor: Colors.white10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12))),
           textTheme: TextTheme(
-            bodyText1: TextStyle(color: Colors.white, fontSize: 24),
-            bodyText2: TextStyle(color: Colors.white54, fontSize: 18),
+            bodyLarge: TextStyle(color: Colors.white, fontSize: 32),
+            bodyMedium: TextStyle(color: Colors.white, fontSize: 24),
+            bodySmall: TextStyle(color: Colors.white, fontSize: 18),
           )),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/home',
+      initialRoute: '/',
       routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => MultiProvider(
+        '/': (BuildContext context) => MultiProvider(
               providers: [
                 Provider<PaginationBloc>(
                   create: (context) => PaginationBloc(),
