2a5baab5a7018dbe6559f7796b96daeae5e86fa1
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-10-06 02:50:29 +0900

    :sparkles: Fix item add button

--- lib/ui/components/templates/add_modal_button.dart
+++ lib/ui/components/templates/add_modal_button.dart
@@ -6,10 +6,9 @@ import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
 import 'package:subsuke/blocs/edit_screen_bloc.dart';
 import 'package:subsuke/blocs/payment_methods_bloc.dart';
-import 'package:subsuke/blocs/subscription_item_bloc.dart';
 import 'package:subsuke/ui/pages/add/add.dart';
 
-class AddModelButtonIOS extends StatelessWidget {
+class AddModelButton extends StatelessWidget {
   IconData getPlatformIconData() {
     if (Platform.isIOS) {
       return CupertinoIcons.plus;
@@ -20,10 +19,13 @@ class AddModelButtonIOS extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final item = Provider.of<SubscriptionItemBLoC>(context);
     return CupertinoButton(
       padding: EdgeInsets.zero,
-      child: Icon(CupertinoIcons.plus, size: 28, color: Theme.of(context).iconTheme.color),
+      child: Icon(
+        CupertinoIcons.plus,
+        size: 28,
+        color: Theme.of(context).iconTheme.color,
+      ),
       onPressed: () {
         showCupertinoModalBottomSheet(
           context: context,
@@ -40,38 +42,7 @@ class AddModelButtonIOS extends StatelessWidget {
                 dispose: (context, bloc) => bloc.dispose(),
               ),
             ],
-            child: AddPage(() => item.getItems()),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class AddModelButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final item = Provider.of<SubscriptionItemBLoC>(context);
-    return IconButton(
-      icon: Icon(Icons.add, size: 28),
-      splashRadius: 22,
-      onPressed: () {
-        showCupertinoModalBottomSheet(
-          context: context,
-          expand: true,
-          useRootNavigator: true,
-          builder: (BuildContext context) => MultiProvider(
-            providers: [
-              Provider<EditScreenBLoC>(
-                create: (context) => EditScreenBLoC(),
-                dispose: (context, bloc) => bloc.dispose(),
-              ),
-              Provider<PaymentMethodBLoC>(
-                create: (context) => PaymentMethodBLoC(),
-                dispose: (context, bloc) => bloc.dispose(),
-              ),
-            ],
-            child: AddPage(() => item.getItems()),
+            child: AddPage(),
           ),
         );
       },
