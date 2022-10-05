de96a1b3bc8e58e3de06a1e375b602273a5b674d
Yusuke Shimizu <44394399+ushmz@users.noreply.github.com>
2022-10-06 02:48:41 +0900

    :sparkles: Fix item list screen to use shared Widget

--- lib/ui/pages/list/list.ios.dart
+++ lib/ui/pages/list/list.ios.dart
@@ -1,17 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:provider/provider.dart';
-import 'package:subsuke/blocs/edit_screen_bloc.dart';
-import 'package:subsuke/blocs/payment_methods_bloc.dart';
 import 'package:subsuke/blocs/subscription_item_bloc.dart';
 import 'package:subsuke/models/subsc.dart';
-import 'package:subsuke/ui/components/ui_parts/subscription_list_item.dart';
 import 'package:subsuke/ui/components/templates/add_modal_button.dart';
-import 'package:subsuke/ui/pages/edit/edit.dart';
-import 'package:subsuke/ui/pages/list/sort_icon_button.dart';
 import 'package:subsuke/ui/components/templates/price_carousel.dart';
+import 'package:subsuke/ui/pages/list/list_item.dart';
+import 'package:subsuke/ui/pages/list/sort_icon_button.dart';
 
 class ListPageIOS extends StatelessWidget {
   @override
@@ -25,6 +20,9 @@ class ListPageIOS extends StatelessWidget {
           case ConnectionState.waiting:
             return Center(child: CircularProgressIndicator());
           default:
+            if (ss.data == null) {
+              return Center(child: Text("サブスクリプションを追加"));
+            }
             return CupertinoPageScaffold(
               child: CustomScrollView(
                 slivers: [
@@ -57,56 +55,11 @@ class ListPageIOS extends StatelessWidget {
                       [
                         Column(
                           children: ss.data!.map((item) {
-                            return Slidable(
-                              key: ValueKey(item.id),
-                              endActionPane: ActionPane(
-                                extentRatio: 0.2,
-                                motion: BehindMotion(),
-                                children: [
-                                  SlidableAction(
-                                    flex: 1,
-                                    onPressed: (BuildContext c) =>
-                                        bloc.delete(item.id),
-                                    backgroundColor:
-                                        Theme.of(context).primaryColor,
-                                    icon: Icons.delete,
-                                    label: "削除",
-                                  )
-                                ],
-                              ),
-                              child: Material(
-                                type: MaterialType.transparency,
-                                child: InkWell(
-                                  onTap: () {
-                                    showCupertinoModalBottomSheet(
-                                      context: context,
-                                      builder: (BuildContext ctx) {
-                                        return MultiProvider(
-                                          providers: [
-                                            Provider<EditScreenBLoC>(
-                                              create: (ctx) => EditScreenBLoC(),
-                                              dispose: (ctx, bloc) =>
-                                                  bloc.dispose(),
-                                            ),
-                                            Provider<PaymentMethodBLoC>(
-                                              create: (context) =>
-                                                  PaymentMethodBLoC(),
-                                              dispose: (context, bloc) =>
-                                                  bloc.dispose(),
-                                            ),
-                                          ],
-                                          child: EditPage(
-                                              item, () => bloc.getItems()),
-                                        );
-                                      },
-                                    );
-                                  },
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 2),
-                                    child: SubscriptionListItem(item),
-                                  ),
-                                ),
-                              ),
+                            return ListItem(
+                              item: item,
+                              onSlidableActionPressed: (BuildContext c) {
+                                bloc.delete(item.id);
+                              },
                             );
                           }).toList(),
                         )
