fa8c6c0123a23781ff682853f04186bc750af052
ushmz <44394399+ushmz@users.noreply.github.com>
2022-10-10 18:48:47 +0900

    :sparkles: Update carousel

--- lib/ui/components/templates/price_carousel.dart
+++ lib/ui/components/templates/price_carousel.dart
@@ -4,55 +4,57 @@ import 'package:subsuke/ui/components/ui_parts/price_card.dart';
 
 class PriceCarousel extends StatelessWidget {
   final List<SubscriptionItem> items;
+  final int defaultTab;
 
-  PriceCarousel({required this.items});
+  PriceCarousel({required this.items, this.defaultTab = 0});
 
-  List<Widget> priceInfoBuilder(
-      BuildContext context, List<SubscriptionItem> items) {
+  List<Widget> childBuilder(BuildContext ctx, List<SubscriptionItem> items) {
     int daily = 0;
     int weekly = 0;
     int monthly = 0;
     int yearly = 0;
 
-    items.forEach((item) {
-      switch (item.interval) {
-        case PaymentInterval.Daily:
-          final y = item.price * 365;
-          daily += item.price;
-          weekly += item.price * 7;
-          monthly += y ~/ 12;
-          yearly += y;
-          break;
-        case PaymentInterval.Weekly:
-          final y = item.price ~/ 7 * 365;
-          daily += item.price ~/ 7;
-          weekly += item.price;
-          monthly += y ~/ 12;
-          yearly += y;
-          break;
-        case PaymentInterval.Fortnightly:
-          final y = item.price ~/ 14 * 365;
-          daily += item.price ~/ 14;
-          weekly += item.price ~/ 2;
-          monthly += y ~/ 12;
-          yearly += y;
-          break;
-        case PaymentInterval.Monthly:
-          final d = item.price * 12 ~/ 365;
-          daily += d;
-          weekly += d * 7;
-          monthly += item.price;
-          yearly += item.price * 12;
-          break;
-        case PaymentInterval.Yearly:
-          final d = item.price ~/ 365;
-          daily += d;
-          weekly += d * 7;
-          monthly += item.price ~/ 12;
-          yearly += item.price;
-          break;
-      }
-    });
+    items.forEach(
+      (item) {
+        switch (item.interval) {
+          case PaymentInterval.Daily:
+            final y = item.price * 365;
+            daily += item.price;
+            weekly += item.price * 7;
+            monthly += y ~/ 12;
+            yearly += y;
+            break;
+          case PaymentInterval.Weekly:
+            final y = item.price ~/ 7 * 365;
+            daily += item.price ~/ 7;
+            weekly += item.price;
+            monthly += y ~/ 12;
+            yearly += y;
+            break;
+          case PaymentInterval.Fortnightly:
+            final y = item.price ~/ 14 * 365;
+            daily += item.price ~/ 14;
+            weekly += item.price ~/ 2;
+            monthly += y ~/ 12;
+            yearly += y;
+            break;
+          case PaymentInterval.Monthly:
+            final d = item.price * 12 ~/ 365;
+            daily += d;
+            weekly += d * 7;
+            monthly += item.price;
+            yearly += item.price * 12;
+            break;
+          case PaymentInterval.Yearly:
+            final d = item.price ~/ 365;
+            daily += d;
+            weekly += d * 7;
+            monthly += item.price ~/ 12;
+            yearly += item.price;
+            break;
+        }
+      },
+    );
 
     return [
       PriceCard(daily, PaymentInterval.Daily),
@@ -67,7 +69,7 @@ class PriceCarousel extends StatelessWidget {
                 decoration: BoxDecoration(
                   boxShadow: [
                     BoxShadow(
-                      color: Theme.of(context).cardTheme.shadowColor!,
+                      color: Theme.of(ctx).cardTheme.shadowColor!,
                       spreadRadius: 0.1,
                       blurRadius: 7.0,
                       offset: Offset(0, 5.0),
@@ -83,25 +85,25 @@ class PriceCarousel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
+      initialIndex: defaultTab,
       length: 4,
-      child: Container(
-        child: Padding(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 5,
-                child: TabBarView(
-                  children: priceInfoBuilder(context, items),
-                ),
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: TabBarView(
+                children: childBuilder(context, items),
               ),
-              Expanded(
-                flex: 1,
-                child: TabPageSelector(
-                    selectedColor: Theme.of(context).primaryColor),
+            ),
+            Expanded(
+              flex: 1,
+              child: TabPageSelector(
+                selectedColor: Theme.of(context).primaryColor,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
