382c3aa04bb567b53476b0efa998662896b044cc
onikun94 <onikun94@gmail.com>
2022-11-10 08:47:41 +0900

    basic set

--- test/check_times_test.dart
+++ test/check_times_test.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:subsuke/ui/components/ui_parts/app_bar.dart';
+
 import 'package:subsuke/ui/pages/config/config.ios.dart';
 
-import 'package:timezone/data/latest_all.dart' as tz;
-import 'package:timezone/timezone.dart' as timezone;
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-
 void main() {
-  testWidgets('app bar confirm test', (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: MainAppBar(),
-    ));
-    expect(find.text("Subsuke"), findsOneWidget);
+  testWidgets("check time test", (WidgetTester tester) async {
+    await [
+      tester.pumpWidget(MaterialApp(
+          home: PaymentReminderSchedulePickerRow(
+        pickedValue: TimeOfDay(hour: 12, minute: 0),
+        onChange: ((val) {
+          print(val);
+        }),
+      )
+
+          // home: ReminderTestButtonRow(send: notification),
+          ))
+    ];
+    // await tester.tap(find.text("リマインダーの時刻"));
+    // await tester.pump();
+    expect(find.text("リマインダーの時刻"), findsOneWidget);
   });
 }
