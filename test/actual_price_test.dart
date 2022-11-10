72a88e6450eff77fc128c4d5c1a51c9eccff8ecc
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2022-11-10 18:13:43 +0900

    アイテム編集テスト（途中）

--- test/actual_price_test.dart
+++ test/actual_price_test.dart
@@ -25,3 +25,5 @@ void actualPriceTest() {
     });
   }
 }
+
+
--- test/edit_item_test.dart
+++ test/edit_item_test.dart
@@ -1,11 +1,22 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:subsuke/models/actual_price.dart';
 import 'package:subsuke/models/subscription_item.dart';
+import 'package:subsuke/blocs/subscription_item_bloc.dart';
 
 import 'fixture.dart';
 
 void main() {
-  test('edit_item_test', () async {
-    expect(1, 1);
-  });
+  final fixture = getSubscriptionItemsFixtures();
+  final tt = fixture['tests'] as List<dynamic>;
+  for (var t in tt) {
+    group("Test edit item", () {
+      test("> ${t['description']}", () async {
+        final items = t['items'] as List<dynamic>;
+        final i = items.map((e) => SubscriptionItem.fromJson(e)).toList();
+        final bloc = SubscriptionItemBLoC();
+        bloc.setSubscriptionItems(i);
+        bloc.update(t['item']['id'], t['item']);
+      });
+    });
+  }
 }
