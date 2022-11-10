f331730bc46eff233f528e314039d9abcc3a142e
onikun94 <onikun94@gmail.com>
2022-11-10 16:47:44 +0900

    add comma

--- test/check_times_test.dart
+++ test/check_times_test.dart
@@ -5,12 +5,14 @@ import 'package:subsuke/ui/pages/config/config.ios.dart';
 
 void main() {
   testWidgets("check time test", (WidgetTester tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: PaymentReminderSchedulePickerRow(
-        pickedValue: TimeOfDay(hour: 12, minute: 11),
-        onChange: ((val) {}),
+    await tester.pumpWidget(
+      MaterialApp(
+        home: PaymentReminderSchedulePickerRow(
+          pickedValue: TimeOfDay(hour: 12, minute: 11),
+          onChange: ((val) {}),
+        ),
       ),
-    ));
+    );
     expect(find.text("12:11"), findsOneWidget);
   });
 }
