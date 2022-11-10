95cee8aff6bfdf606a3e253d1e7bb871c14485dd
onikun <71135834+onikun94@users.noreply.github.com>
2022-11-10 16:58:31 +0900

    時刻の変更が反映されるかのテスト (#58)
    * delete default widget_test
    
    * add change times test
    
    * basic set
    
    * confirm changed tiime
    
    * add comma

---
+++ test/check_times_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:subsuke/ui/pages/config/config.ios.dart';
+
+void main() {
+  testWidgets("check time test", (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: PaymentReminderSchedulePickerRow(
+          pickedValue: TimeOfDay(hour: 12, minute: 11),
+          onChange: ((val) {}),
+        ),
+      ),
+    );
+    expect(find.text("12:11"), findsOneWidget);
+  });
+}
