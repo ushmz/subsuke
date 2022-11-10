65385d0659452f0e0544143f5dfccd8f288d77e6
nightshrine <yamamoto.shunsuke.20@shizuoka.ac.jp>
2022-11-10 17:42:36 +0900

    edit-item-test first commit

---
+++ test/edit_item_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:subsuke/models/actual_price.dart';
+import 'package:subsuke/models/subscription_item.dart';
+
+import 'fixture.dart';
+
+void main() {
+  test('edit_item_test', () async {
+    expect(1, 1);
+  });
+}
