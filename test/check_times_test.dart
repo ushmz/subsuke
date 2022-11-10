d91d26688c6103652f5c233e53b5631085baf86c
onikun94 <onikun94@gmail.com>
2022-11-10 09:28:59 +0900

    confirm changed tiime

--- test/check_times_test.dart
+++ test/check_times_test.dart
@@ -5,20 +5,12 @@ import 'package:subsuke/ui/pages/config/config.ios.dart';
 
 void main() {
   testWidgets("check time test", (WidgetTester tester) async {
-    await [
-      tester.pumpWidget(MaterialApp(
-          home: PaymentReminderSchedulePickerRow(
-        pickedValue: TimeOfDay(hour: 12, minute: 0),
-        onChange: ((val) {
-          print(val);
-        }),
-      )
-
-          // home: ReminderTestButtonRow(send: notification),
-          ))
-    ];
-    // await tester.tap(find.text("リマインダーの時刻"));
-    // await tester.pump();
-    expect(find.text("リマインダーの時刻"), findsOneWidget);
+    await tester.pumpWidget(MaterialApp(
+      home: PaymentReminderSchedulePickerRow(
+        pickedValue: TimeOfDay(hour: 12, minute: 11),
+        onChange: ((val) {}),
+      ),
+    ));
+    expect(find.text("12:11"), findsOneWidget);
   });
 }
